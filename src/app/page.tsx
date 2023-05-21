d4e17f046ed22a87a66169469fbd3401395b2b9f
kavis777 <wwrd0129@gmail.com>
2023-05-21 19:00:17 +0900

    main feat:チェックボックスのサンプルコードを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,63 @@
 
 import { useState } from "react";
 
-type GreetingProps = {
-  name: string;
-};
-
-const Greeting: React.FC<GreetingProps> = ({ name }) => {
-  console.log("Greeting was rendered at", new Date().toLocaleTimeString());
-  return (
-    <h3>
-      Hello{name && ", "}
-      {name}!
-    </h3>
-  );
-};
-
 export default function Home() {
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
+  const [checkItems, setCheckItems] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
+  const toggle = (value: string) => {
+    const result = [...checkItems];
+    if (checkItems.includes(value)) {
+      const index = checkItems.indexOf(value);
+      result.splice(index, 1);
+    } else {
+      result.push(value);
+    }
+    setCheckItems(result);
+    setMessage(result.join(","));
+  };
+
+  // 1から100までの数値を持つ配列
+  // [1, 2, 3, ..., 10]
+  const array = Array.from({ length: 10 }, (_, index) => index + 1);
 
   return (
     <>
-      <label>
-        Name{": "}
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-      </label>
-      <label>
-        Address{": "}
-        <input value={address} onChange={(e) => setAddress(e.target.value)} />
-      </label>
-      <Greeting name={name} />
+      <h2>React memoを使わない場合</h2>
+      {array.map((number) => (
+        <CheckBox
+          key={number}
+          checked={checkItems.includes(`value${number}`)}
+          value={`value${number}`}
+          toggle={toggle}
+        >
+          {`条件：${number}`}
+        </CheckBox>
+      ))}
+      <br />
+      <div>検索条件：{message}</div>
+      <div></div>
     </>
   );
 }
+
+type CheckBoxProps = {
+  checked: boolean;
+  value: string;
+  children: React.ReactNode;
+  toggle: (value: string) => void;
+};
+
+const CheckBox = ({ children, checked, value, toggle }: CheckBoxProps) => {
+  return (
+    <div>
+      <input type="checkbox" checked={checked} readOnly />
+      <label
+        onClick={() => {
+          toggle(value);
+        }}
+      >
+        <span>{children}</span>
+      </label>
+    </div>
+  );
+};
